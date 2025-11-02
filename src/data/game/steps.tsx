8b1315175897e8f2d1c5817734dfbd4d8125c8c5
Bartosz Bazański <bazanski.bartosz@gmail.com>
2025-11-02 16:13:26 +0100

    feat: add new options

--- src/data/game/steps.tsx
+++ src/data/game/steps.tsx
@@ -7,6 +7,7 @@ import Face4 from "@/assets/faces/face-4.svg";
 import Face5 from "@/assets/faces/face-5.svg";
 import Face6 from "@/assets/faces/face-6.svg";
 import Face7 from "@/assets/faces/face-7.svg";
+import Face8 from "@/assets/faces/face-8.svg";
 
 import Eyes1 from "@/assets/eyes/eyes-1.svg";
 import Eyes2 from "@/assets/eyes/eyes-2.svg";
@@ -14,6 +15,8 @@ import Eyes3 from "@/assets/eyes/eyes-3.svg";
 import Eyes4 from "@/assets/eyes/eyes-4.svg";
 import Eyes5 from "@/assets/eyes/eyes-5.svg";
 import Eyes6 from "@/assets/eyes/eyes-6.svg";
+import Eyes7 from "@/assets/eyes/eyes-7.svg";
+import Eyes8 from "@/assets/eyes/eyes-8.svg";
 
 import Nose1 from "@/assets/noses/nose-1.svg";
 import Nose2 from "@/assets/noses/nose-2.svg";
@@ -22,6 +25,8 @@ import Nose4 from "@/assets/noses/nose-4.svg";
 import Nose5 from "@/assets/noses/nose-5.svg";
 import Nose6 from "@/assets/noses/nose-6.svg";
 import Nose7 from "@/assets/noses/nose-7.svg";
+import Nose8 from "@/assets/noses/nose-8.svg";
+import Nose9 from "@/assets/noses/nose-9.svg";
 
 import Mouth1 from "@/assets/mouths/mouth-1.svg";
 import Mouth2 from "@/assets/mouths/mouth-2.svg";
@@ -29,6 +34,7 @@ import Mouth3 from "@/assets/mouths/mouth-3.svg";
 import Mouth4 from "@/assets/mouths/mouth-4.svg";
 import Mouth5 from "@/assets/mouths/mouth-5.svg";
 import Mouth6 from "@/assets/mouths/mouth-6.svg";
+import Mouth7 from "@/assets/mouths/mouth-7.svg";
 
 import Hairstyle1 from "@/assets/hairstyles/hairstyle-1.svg";
 import Hairstyle2 from "@/assets/hairstyles/hairstyle-2.svg";
@@ -37,6 +43,7 @@ import Hairstyle4 from "@/assets/hairstyles/hairstyle-4.svg";
 import Hairstyle5 from "@/assets/hairstyles/hairstyle-5.svg";
 import Hairstyle6 from "@/assets/hairstyles/hairstyle-6.svg";
 import Hairstyle7 from "@/assets/hairstyles/hairstyle-7.svg";
+import Hairstyle8 from "@/assets/hairstyles/hairstyle-8.svg";
 
 export const iconsMap = {
   face: {
@@ -46,7 +53,8 @@ export const iconsMap = {
     face4: Face4,
     face5: Face5,
     face6: Face6,
-    face7: Face7
+    face7: Face7,
+    face8: Face8,
   },
   eyes: {
     eyes1: Eyes1,
@@ -54,7 +62,9 @@ export const iconsMap = {
     eyes3: Eyes3,
     eyes4: Eyes4,
     eyes5: Eyes5,
-    eyes6: Eyes6
+    eyes6: Eyes6,
+    eyes7: Eyes7,
+    eyes8: Eyes8,
   },
   nose: {
     nose1: Nose1,
@@ -64,6 +74,8 @@ export const iconsMap = {
     nose5: Nose5,
     nose6: Nose6,
     nose7: Nose7,
+    nose8: Nose8,
+    nose9: Nose9,
   },
   mouth: {
     mouth1: Mouth1,
@@ -71,7 +83,8 @@ export const iconsMap = {
     mouth3: Mouth3,
     mouth4: Mouth4,
     mouth5: Mouth5,
-    mouth6: Mouth6
+    mouth6: Mouth6,
+    mouth7: Mouth7,
   },
   hairstyle: {
     hairstyle1: Hairstyle1,
@@ -81,6 +94,7 @@ export const iconsMap = {
     hairstyle5: Hairstyle5,
     hairstyle6: Hairstyle6,
     hairstyle7: Hairstyle7,
+    hairstyle8: Hairstyle8,
   }
 } as const;
 
